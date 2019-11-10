ad911412c3870bf690a752ed28a64b402eee1c6f
geekyJock8 <vikas.rajput54@gmail.com>
2019-11-10 22:42:28 +0530

    Added CORS

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
+from flask_cors import CORS, cross_origin
 import json
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 
